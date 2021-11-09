71b7b9b2a5261fff6be642e5abfa2c27c664764c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2021-11-09 14:08:46 +0100

    Add draggable icon

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
+import DragSrc from './assets/drag.png';
 
 const StyledRow = styled.div`
   border: 1px solid black;
   margin: 0.5rem auto;
   width: 300px;
   padding: 0.5rem;
+  display: flex;
+  align-items: center;
+
+  .dragIcon {
+    width: 1.5rem;
+    height: 1.5rem;
+    display: block;
+    margin-right: 2rem;
+    background: url(${DragSrc}) no-repeat;
+    background-size: contain;
+    cursor: pointer;
+  }
 `;
 
 const Row = ({ id, text, handleDrag, handleDrop }) => {
+  console.log('DragSrc', DragSrc);
   return (
     <StyledRow
       id={id}
@@ -16,7 +30,8 @@ const Row = ({ id, text, handleDrag, handleDrop }) => {
       onDragStart={handleDrag}
       onDrop={handleDrop}
     >
-      {text}
+      <span className="dragIcon" />
+      <span>{text}</span>
     </StyledRow>
   );
 };
